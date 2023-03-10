28695416e487fdea6d53d38b8f5b2f249367a8fb
xr1s0nx <artem.gumerov.05@gmail.com>
2023-03-11 01:22:48 +0300

    dz

---
+++ child/child/child/child.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	cout << argc << endl;
+
+	int out = 0;
+	
+	for (int i = 1; i < argc; i++)
+	{
+		out += atoi(argv[i]);
+	}
+
+	cout << out;
+}
---
+++ homework-num1/homework-num1/homework-num1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(STARTUPINFO));
+    si.cb = sizeof(STARTUPINFO);
+
+    string path = "C:\\Users\\artem\\OneDrive\\Documents\\mdk\\win api\\child\\child\\x64\\Debug\\child.exe ";
+
+    string args;
+    cout << "Введите аргументы -> ";
+    getline(cin, args);
+
+    path = path + " " + args;
+
+    char process[MAX_PATH];
+
+    strcpy_s(process, path.c_str());
+
+    BOOL isStart = CreateProcess(NULL, process, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+}
